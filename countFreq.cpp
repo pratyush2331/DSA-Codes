21aa2443add65bf63792fff5a7a9181c8bf5287e
Pratyush Raj <72189926+pratyush2331@users.noreply.github.com>
2022-10-27 00:38:34 +0530

    Add files via upload

---
+++ countFreq.cpp
@@ -0,0 +1,32 @@
+// 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void countFreq(int arr[], int n) {
+    vector<bool> visited(n, false);
+
+    for(int i = 0; i < n; i++) {
+        int cnt = 1;
+        if(!visited[i]) {
+            for(int j = i+1; j < n; j++) {
+                if(arr[i] == arr[j]) {
+                    visited[j] = true;
+                    cnt++;
+                }
+            }
+            cout << arr[i] << " " << cnt << endl;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 5, 10, 15, 10, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    countFreq(arr, n);
+
+    return 0;
+}
